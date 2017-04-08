c1a0dfb21081a5320de55bbf66981d7fb1428ce4
tjacksonsmith <smitim12@evergreen.edu>
2017-04-08 12:47:35 -0700

    Add files via upload

---
+++ Week1_Cycling_Window_Code.m
@@ -0,0 +1,25 @@
+% Timothy Smith
+% Choosing Wondows FFT analyzer 
+% Uniform Window
+% 50.125 kHz and 50 kHz frequency
+
+figure
+% 50.125 kHz
+subplot(2,1,1)
+plot(Frequency_3,Voltage_3,'m')
+xlabel('Frequency')
+ylabel('Voltage')
+title( 'Uniform Window 50.125kHz')
+dim = [0.15, 0.6 0.3 0.3];
+str = {'Span=100 kHz','Linewidth=250 kHz','Acq-time=4ms'};
+a=annotation('textbox',dim,'String',str,'FitBoxToText','on');
+a.FontSize=8;
+subplot(2,1,2)
+plot(Frequency_4,Voltage_4,'c')
+xlabel('Frequency')
+ylabel('Voltage')
+title( 'Uniform Window 50.250kHz')
+dim = [0.15, 0.125 0.3 0.3];
+str = {'Span=100 kHz','Linewidth=250 kHz','Acq-time=4ms'};
+a=annotation('textbox',dim,'String',str,'FitBoxToText','on');
+a.FontSize=8;
